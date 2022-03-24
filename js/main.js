e1f6c836b78fa7c2563c736df016a5d7f59aca36
Cédric Dujardin <51912624+cdujardin4000@users.noreply.github.com>
2022-03-24 21:59:35 +0100

    Probleme sur chrome pour changer le logo ???

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ $(function(){
     NORESULTS.css({'font-size': '2rem', 'margin': '2rem 0'});
     INPUT.css({'margin-bottom': '1rem'});
     $('.copyright').css({'text-align': 'center', 'color': '#ffca2c'});
-    $('.mail:hover').css({'color': '#ffca2c'});
+    $('a.mail').css({'color': 'white'});
+    $('a:hover.mail ').css({'color': '#ffca2c'});
     $('.change-logo').css({'margin-top': '1rem'});
     $('label.statusMessage').css({'margin': '2rem 0','font-size': '2rem'});
     const TARGETSLIDER = $('.sliderTarget');
