7a14388811bbc95aa5b2bfaf10cb480f4fd27a41
Cédric Dujardin <51912624+cdujardin4000@users.noreply.github.com>
2022-03-24 23:06:45 +0100

    ok c'etait le cache

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 $(function(){
 
-    const FOOTER = $('footer');
-    const NORESULTS = $('.no-result');
-    const INPUT = $('#input');
-    FOOTER.css({'padding-top': '2rem', 'padding-bottom': '2rem', 'margin-top': '2rem'});
-    NORESULTS.css({'font-size': '2rem', 'margin': '2rem 0'});
-    INPUT.css({'margin-bottom': '1rem'});
+    $('header').css({'margin-bottom': '2rem'})
+    $('footer').css({'padding-top': '2rem', 'padding-bottom': '2rem', 'margin-top': '2rem'});
+    $('.no-result').css({'font-size': '2rem', 'margin': '2rem 0'});
+    $('#input').css({'margin-bottom': '1rem'});
     $('.copyright').css({'text-align': 'center', 'color': '#ffca2c'});
     $('a.mail').css({'color': 'white'});
-    $('a:hover.mail ').css({'color': '#ffca2c'});
+    $('.copyright > a:hover ').css({'color': '#ffca2c'});
     $('.change-logo').css({'margin-top': '1rem'});
     $('label.statusMessage').css({'margin': '2rem 0','font-size': '2rem'});
     const TARGETSLIDER = $('.sliderTarget');
